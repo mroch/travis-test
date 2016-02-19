cd216c4aaecb04b195f91569d35aaec8ff2b0647
Marshall Roch <mroch@fb.com>
2016-02-19 13:50:03 -0800

    try before_deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,15 @@
 #!/bin/bash -ex
 
 which rvm
+printf "travis_fold:start:rvm_info_1\n"
 rvm info
+printf "travis_fold:end:rvm_info_1\n"
+printf "travis_fold:start:source_rvm\n"
 source "$HOME/.rvm/scripts/rvm"
+printf "travis_fold:end:source_rvm\n"
+printf "travis_fold:start:rvm_use\n"
 rvm use 2.2 --binary --install
+printf "travis_fold:end:rvm_use\n"
+printf "travis_fold:start:rvm_info_2\n"
 rvm info
+printf "travis_fold:end:rvm_info_2\n"
